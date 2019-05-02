82c008267c24f10c3027a2467cee2ac54941c40c
hajnalandor <hajnal.andor1993@gmail.com>
2019-05-02 15:38:23 +0200

    first commit

---
+++ shoppingList/src/main/java/hu/flowacademy/shoppingList/domain/shoppingList.java
@@ -0,0 +1,68 @@
+package hu.flowacademy.shoppingList.domain;
+
+public class shoppingList {
+    private String id;
+    private String name;
+    private String cathegory;
+    private int quantity;
+    private int quantityOne;
+    private int price;
+    private String description;
+
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCathegory() {
+        return cathegory;
+    }
+
+    public void setCathegory(String cathegory) {
+        this.cathegory = cathegory;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantityOne() {
+        return quantityOne;
+    }
+
+    public void setQuantityOne(int quantityOne) {
+        this.quantityOne = quantityOne;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
